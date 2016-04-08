3062653165ddbaa9b8f96a6d01fc96ba34792df9
Syohei YOSHIDA <syohex@gmail.com>
2016-04-09 00:51:34 +0900

    Add Code: meta comment

--- cql-mode.el
+++ cql-mode.el
@@ -20,6 +20,8 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Code:
+
 (require 'sql)
 
 (defvar cql-mode-font-lock-keywords
