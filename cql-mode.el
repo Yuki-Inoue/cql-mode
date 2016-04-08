bb368b9817ef7de737d4b7edbcfec60ab2813462
Yuki Inoue <inouetakahiroki@gmail.com>
2016-04-08 23:55:47 +0900

    Make cql-mode a package

--- cql-mode.el
+++ cql-mode.el
@@ -1,3 +1,24 @@
+;;; cql-mode.el --- Major mode for editting CQLs
+
+;; Copyright (C) 2016 Yuki Inoue
+
+;; Author: Yuki Inoue <inouetakahiroki at gmail.com>
+;; Version: 0.0.1
+;; Keywords: cql, cassandra
+;; URL: https://github.com/Yuki-Inoue/cql-mode
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'sql)
 
@@ -65,3 +86,7 @@
 ;
 ; (sql-set-product-feature 'cassandra
 ;                          :sqli-program 'cassandra-program)
+
+(provide 'cql-mode)
+
+;;; cql-mode.el ends here
